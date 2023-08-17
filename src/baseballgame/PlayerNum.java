cce8e71f87d35e175d042ddb7502fb3848dff1c5
siwol25 <cyj5695@naver.com>
2023-08-17 15:41:40 +0900

    Add PlayerNum

--- src/baseballgame/PlayerNum.java
+++ src/baseballgame/PlayerNum.java
@@ -6,15 +6,23 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BaseballGame {
-    public static void main(String[] args) throws IOException {
+public class PlayerNum {
+    public String inputNum() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String playerNum = br.readLine();
+        return playerNum;
+
+    }
+
+    public List<Integer> createPlayerNum() throws IOException{
+        String playerNum = inputNum();
         List<Integer> playerNums = new ArrayList<>();
 
         for (int i=0; i<playerNum.length(); i++){
+            playerNums.add(Integer.parseInt(playerNum.charAt(i)+""));
 
         }
 
+        return playerNums;
     }
 }
