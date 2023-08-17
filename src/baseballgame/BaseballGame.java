75054ebb4845816892b6a3361e68b5f32a196c39
siwol25 <cyj5695@naver.com>
2023-08-17 16:30:22 +0900

    Add BaseballGame

--- src/baseballgame/BaseballGame.java
+++ src/baseballgame/BaseballGame.java
@@ -5,16 +5,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BaseballGame {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String playerNum = br.readLine();
-        List<Integer> playerNums = new ArrayList<>();
 
-        for (int i=0; i<playerNum.length(); i++){
+        RandomNum randomNum = new RandomNum();
+        PlayerNum playerNum = new PlayerNum();
+        List<Integer> computerNums = randomNum.create();
+        Scanner scanner = new Scanner(System.in);
 
+        while (true){
+            List<Integer> playerNums = playerNum.createPlayerNum();
+            int strikeCount = 0;
+            int ballCount = 0;
+
+            for (int i=0; i<playerNums.size(); i++){
+                int pNum= playerNums.get(i);
+                if (pNum == computerNums.get(i)){
+                    strikeCount++;
+                    continue;
+                }
+
+                if (computerNums.contains(pNum)){
+                    ballCount++;
+                }
+            }
+
+            String result = ballCount > 0 ? ballCount + "볼 " : "";
+            result += strikeCount > 0 ? strikeCount +"스트라이크" : "";
+
+            if (strikeCount == 0 && ballCount == 0){
+                result = "낫싱";
+            }
+
+            System.out.println(result);
+
+            if(strikeCount == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int command = scanner.nextInt();
+
+                if (command == 2){
+                    break;
+                } else if (command == 1) {
+                    computerNums = randomNum.create();
+                }else {
+                    throw new IllegalArgumentException("잘못된 커맨드입니다.");
+                }
+
+            }
         }
+
 
     }
 }
--- src/baseballgame/PlayerNum.java
+++ src/baseballgame/PlayerNum.java
@@ -9,7 +9,17 @@ import java.util.List;
 public class PlayerNum {
     public String inputNum() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("숫자를 입력해주세요 : ");
         String playerNum = br.readLine();
+        try {
+            Integer.parseInt(playerNum);
+            if (playerNum.length() != 3){
+                throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
+            }
+        }catch (Exception e){
+            throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
+        }
+
         return playerNum;
 
     }
